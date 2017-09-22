5edd9e9d9812774e1940014b531d1ae6c06a4ba2
Reid Mollway <reidmollway@gmail.com>
2017-09-22 14:57:45 -0400

    Inlcuded config for heroku deployment

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://reidDB:RM921104@ds163053.mlab.com:63053/cen3031',
+  },
+  googleMaps: {
+    key: 'AIzaSyDQeRwb06qpuTpSycvYtmWmqhpdAt70uzU'
+  },
+  port: 8080
+};
