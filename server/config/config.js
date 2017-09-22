402756301ea6eec41bb7ea228cf5deae852ce032
Reid Mollway <reidmollway@gmail.com>
2017-09-22 16:51:43 -0400

    new google maps api key

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     uri: 'mongodb://reidDB:RM921104@ds163053.mlab.com:63053/cen3031',
   },
   googleMaps: {
-    key: 'AIzaSyDQeRwb06qpuTpSycvYtmWmqhpdAt70uzU'
+    key: 'AIzaSyCaBSsP7AmW6DpTQ5zL1GS01FJ1a41YmBc'
   },
   port: process.env.PORT
 };
